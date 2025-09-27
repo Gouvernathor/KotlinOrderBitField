d80a8f1a034353d5ce67299b2dac0fb4b779168b
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-09-27 23:20:29 +0200

    whitespace, doc and reorders

--- ReorderableSet.factory.kt
+++ ReorderableSet.factory.kt
@@ -1,5 +1,7 @@
 package fr.gouvernathor.orderbitfield
 
+// Factory functions
+
 /**
  * Create a new ReorderableSet with the given elements.
  * Use this if you don't intend to manipulate OrderBitField indexes,
@@ -7,6 +9,7 @@ package fr.gouvernathor.orderbitfield
  */
 public fun <E> reorderableSetOf(vararg elements: E): ReorderableSet<E> =
     reorderableSetOf(OrderBitField, *elements)
+
 /**
  * Create a new ReorderableSet with the given elements,
  * allowing you to specify how the OrderValue indexes are created.
@@ -19,25 +22,18 @@ public fun <E, O: OrderValue<O>> reorderableSetOf(
 
 /**
  * Create a new ReorderableSet with the given elements.
- * Use this if you intend to manipulate OrderBitField indexes,
- * for instance if you need to save them or store them in a database,
- * or if the elements contain their own OrderBitField indexes as part of their structure.
+ * Use this if you intend to manipulate the OrderValue indexes,
+ * and if you also want to specify how the OrderValue indexes are created.
  *
  * In that latter case, assuming the property is called "idx",
- * you can use `reorderableSetOf({ it.idx }, { e, c -> e.idx = c }, *elements)`.
+ * you can use `reorderableSetOf({ it.idx }, { e, c -> e.idx = c }, factory, *elements)`.
  *
- * In any case, only the container should ever be tampering with the OrderBitField indexes.
+ * In any case, only the ReorderableSet container should ever be tampering with the OrderValue indexes.
  *
- * Before passing them to this construction function, if you need to initialize the OrderBitField property,
+ * Before passing them to this construction function, if you need to initialize the OrderValue property,
  * you can set it to null or to the empty list ; the getter function will still be safe to call
  * because the constructor and any method will call the setter (with a non-null value) on any incoming element before ever calling the getter.
  */
-public fun <E> reorderableSetOf( // TODO this should probably use the String implem by default
-    getCode: (E) -> OrderBitField,
-    setCode: (E, OrderBitField) -> Unit,
-    vararg elements: E,
-): ReorderableSet<E> =
-    reorderableSetOf(getCode, setCode, OrderBitField, *elements)
 public fun <E, O: OrderValue<O>> reorderableSetOf(
     getCode: (E) -> O,
     setCode: (E, O) -> Unit,
@@ -46,59 +42,98 @@ public fun <E, O: OrderValue<O>> reorderableSetOf(
 ): ReorderableSet<E> =
     elements.toList().toReorderableSet(getCode, setCode, factory)
 
+// TODO add a String-based lambda version
+
+/**
+ * This version skips the need for a factory, using OrderBitField as the OrderValue type.
+ */
+public fun <E> reorderableSetOf(
+    getCode: (E) -> OrderBitField,
+    setCode: (E, OrderBitField) -> Unit,
+    vararg elements: E,
+): ReorderableSet<E> =
+    reorderableSetOf(getCode, setCode, OrderBitField, *elements)
+
+
+
+// Extension methods
+
+// batteries-included version
+
 // avoid converting to List if already a Collection
 public fun <E> Collection<E>.toReorderableSet(): ReorderableSet<E> =
     this.toReorderableSet(OrderBitField)
+
+public fun <E> Iterable<E>.toReorderableSet(): ReorderableSet<E> =
+    this.toReorderableSet(OrderBitField)
+
+public fun <E> Sequence<E>.toReorderableSet(): ReorderableSet<E> =
+    this.toReorderableSet(OrderBitField)
+
+public fun <E> Array<E>.toReorderableSet(): ReorderableSet<E> =
+    this.toReorderableSet(OrderBitField)
+
+// from a factory
+
+// avoid converting to List if already a Collection
 public fun <E, O: OrderValue<O>> Collection<E>.toReorderableSet(
     factory: OrderValueFactory<O>,
 ): ReorderableSet<E> =
     MapBasedReorderableSet(this, factory)
-public fun <E> Iterable<E>.toReorderableSet(): ReorderableSet<E> =
-    this.toReorderableSet(OrderBitField)
+
 public fun <E, O: OrderValue<O>> Iterable<E>.toReorderableSet(
     factory: OrderValueFactory<O>,
 ): ReorderableSet<E> =
     this.toList().toReorderableSet(factory)
-public fun <E> Sequence<E>.toReorderableSet(): ReorderableSet<E> =
-    this.toReorderableSet(OrderBitField)
+
 public fun <E, O: OrderValue<O>> Sequence<E>.toReorderableSet(
     factory: OrderValueFactory<O>,
 ): ReorderableSet<E> =
     this.toList().toReorderableSet(factory)
-public fun <E> Array<E>.toReorderableSet(): ReorderableSet<E> =
-    this.toReorderableSet(OrderBitField)
+
 public fun <E, O: OrderValue<O>> Array<E>.toReorderableSet(
     factory: OrderValueFactory<O>,
 ): ReorderableSet<E> =
     this.toList().toReorderableSet(factory)
+
+// TODO add lambda-based String version
+
+// with OrderBitField implementation, and accessors
 
 public fun <E> Iterable<E>.toReorderableSet(
     getCode: (E) -> OrderBitField,
     setCode: (E, OrderBitField) -> Unit,
 ): ReorderableSet<E> =
     this.toReorderableSet(getCode, setCode, OrderBitField)
+
+public fun <E> Sequence<E>.toReorderableSet(
+    getCode: (E) -> OrderBitField,
+    setCode: (E, OrderBitField) -> Unit,
+): ReorderableSet<E> =
+    this.toReorderableSet(getCode, setCode, OrderBitField)
+
+public fun <E> Array<E>.toReorderableSet(
+    getCode: (E) -> OrderBitField,
+    setCode: (E, OrderBitField) -> Unit,
+): ReorderableSet<E> =
+    this.toReorderableSet(getCode, setCode, OrderBitField)
+
+// from a factory and accessors
+
 public fun <E, O: OrderValue<O>> Iterable<E>.toReorderableSet(
     getCode: (E) -> O,
     setCode: (E, O) -> Unit,
     factory: OrderValueFactory<O>,
 ): ReorderableSet<E> =
     SetLambdaBasedReorderableSet(getCode, setCode, factory, this)
-public fun <E> Sequence<E>.toReorderableSet(
-    getCode: (E) -> OrderBitField,
-    setCode: (E, OrderBitField) -> Unit,
-): ReorderableSet<E> =
-    this.toReorderableSet(getCode, setCode, OrderBitField)
+
 public fun <E, O: OrderValue<O>> Sequence<E>.toReorderableSet(
     getCode: (E) -> O,
     setCode: (E, O) -> Unit,
     factory: OrderValueFactory<O>,
 ): ReorderableSet<E> =
     this.toList().toReorderableSet(getCode, setCode, factory)
-public fun <E> Array<E>.toReorderableSet(
-    getCode: (E) -> OrderBitField,
-    setCode: (E, OrderBitField) -> Unit,
-): ReorderableSet<E> =
-    this.toReorderableSet(getCode, setCode, OrderBitField)
+
 public fun <E, O: OrderValue<O>> Array<E>.toReorderableSet(
     getCode: (E) -> O,
     setCode: (E, O) -> Unit,
